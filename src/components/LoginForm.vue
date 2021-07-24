54ba1c2f14978bf797ab27fc44f746b1351b3501
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-07-24 15:05:04 +0900

    post api

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -56,8 +56,9 @@ export default {
           password: this.password,
         };
         const { data } = await loginUser(userData);
-        console.log(data.user.username);
+        console.log(data);
         this.$store.commit('setUsername', data.user.username);
+        this.$store.commit('setToken', data.token);
         this.$router.push('/main');
         // this.logMessage = `${data.user.username} 님 환영합니다`;
         // this.initForm();
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,37 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <ul>
+        <li v-for="postItem in postItems" :key="postItem._id">
+          {{ postItem.title }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+export default {
+  data() {
+    return {
+      postItems: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const { data } = await fetchPosts();
+        this.postItems = data.posts;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
